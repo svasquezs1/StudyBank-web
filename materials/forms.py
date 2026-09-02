bac4364219d0ff2a9c80dd17af8ca3be9ccb47c3
Gisel Jaramillo Carmona <gljaramilc@eafit.edu.co>
2026-09-01 23:20:18 -0500

    Revert "Implementación RF-05 búsqueda de materiales"

--- materials/forms.py
+++ materials/forms.py
@@ -17,14 +17,3 @@ class MaterialForm(forms.ModelForm):
             'university': forms.Select(),
             'file_type': forms.Select(),
         }
-
-class MaterialSearchForm(forms.Form):
-    q = forms.CharField(
-        required=False,
-        strip=True,
-        widget=forms.TextInput(attrs={
-            'placeholder': 'Buscar por título, materia o descripción...',
-            'class': 'form-control',
-            'type': 'search',
-        })
-    )
--- materials/urls.py
+++ materials/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path,include
+from django.urls import path
 
 from . import views
 
@@ -9,6 +9,4 @@ urlpatterns = [
     path('', views.material_list, name='list'),
     path('<int:pk>/', views.material_detail, name='detail'),
     path('<int:pk>/download/', views.download_material, name='download'),
-    path('search/', views.search_materials, name='search_materials'),
-    
 ]
